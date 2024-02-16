daa23a3b2813c129033ef381aafd67f1e8594142
Michellosky <125925489+MISIIc@users.noreply.github.com>
2024-02-16 16:25:59 +0100

    Add files via upload

---
+++ bot discord bot.py
@@ -0,0 +1,38 @@
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Hai fatto l'accesso come {bot.user}")
+
+@bot.command()
+async def ciao(ctx):
+    await ctx.send(f'Ciao! Sono un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def nuovaPassword(ctx, lunghezza_password = 10):
+    await ctx.send(gen_pass(lunghezza_password))
+
+
+
+def gen_pass(lunghezza_password):
+    elements = "+-/*!&$#?=@<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    password = ""
+
+    for i in range(lunghezza_password):
+        password += random.choice(elements)
+
+    return password
+
+
+bot.run("MTIwMzAyMDYwNDAwMTg4NjIwOQ.GlIfVj.zqErpBoPCd5gE1VZAjk4YudDXNmlN-s4E705CE")
